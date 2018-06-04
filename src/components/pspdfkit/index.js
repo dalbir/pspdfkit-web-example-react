e0327935d83402139f349ee9733726a59ae388b1
PSPDFKit <support@pspdfkit.com>
2018-06-04 12:09:17 +0200

    PSPDFKit for Web React Example

---
+++ src/components/pspdfkit/index.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import PSPDFKitWeb from "pspdfkit";
+
+export default class PSPDFKit extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this._instance = null;
+    this._container = null;
+
+    this.onRef = this.onRef.bind(this);
+    this.load = this.load.bind(this);
+    this.unload = this.unload.bind(this);
+  }
+
+  onRef(container) {
+    this._container = container;
+  }
+
+  async load(props) {
+    console.log(`Loading ${props.pdfUrl}`);
+
+    this._instance = await PSPDFKitWeb.load({
+      pdf: props.pdfUrl,
+      container: this._container,
+      licenseKey: props.licenseKey,
+      baseUrl: props.baseUrl
+    });
+    console.log("Successfully mounted PSPDFKit", this._instance);
+  }
+
+  unload() {
+    PSPDFKitWeb.unload(this._instance || this._container);
+    this._instance = null;
+  }
+
+  componentDidMount() {
+    this.load(this.props);
+  }
+
+  componentDidUpdate(nextProps) {
+    this.unload();
+    this.load(nextProps);
+  }
+
+  componentWillUnmount() {
+    this.unload();
+  }
+
+  render() {
+    return <div ref={this.onRef} style={{ width: "100%", height: "100%", position: "absolute" }} />;
+  }
+}
